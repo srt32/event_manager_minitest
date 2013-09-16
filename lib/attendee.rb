03e4af3add3794ae7e7be1d2ccb0a63ba99063f1
Simon Taranto <simon.taranto@gmail.com>
2013-09-16 11:23:06 -0600

    extract phone_cleaning into method

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,12 +1,24 @@
 class Attendee
-  attr_accessor :first_name
-  attr_accessor :last_name
-  attr_accessor :phone_number
+  attr_accessor :first_name, :last_name, :phone_number
 
   def initialize(input = {})
     @first_name = input[:first_name]
     @last_name = input[:last_name]
-    @phone_number = input[:phone_number]
+    @phone_number = clean_phone_number(input[:phone_number])
+  end
+
+  def clean_phone_number(number)
+    if number
+      number = number.scan(/[0-9]/).join
+      if number.length == 11 && number.start_with?("1")
+        number = number[1..-1]
+      end
+      if number.length != 10
+        number = "0000000000"
+      end
+
+      return number
+    end
   end
 
 end
--- test/attendee_test.rb
+++ test/attendee_test.rb
@@ -41,4 +41,30 @@ class AttendeeTest < MiniTest::Test
     assert_equal "1231234567", attendee.phone_number
   end
 
+  def test_it_cleans_up_phone_numbers_with_periods_and_hyphens
+    attendee = Attendee.new(:phone_number => "202.444-9382")
+    assert_equal "2024449382", attendee.phone_number
+  end
+
+  def test_it_cleans_up_phone_numbers_with_spaces_and_parentheses
+    attendee = Attendee.new(:phone_number => "(202) 444 9382")
+    assert_equal "2024449382", attendee.phone_number
+  end
+
+  def test_it_removes_leading_one_from_an_eleven_digit_phone_number
+    attendee = Attendee.new(:phone_number => "12024449382")
+    assert_equal "2024449382", attendee.phone_number
+  end
+
+  def test_it_throws_away_phone_numbers_that_are_too_long
+    attendee = Attendee.new(:phone_number => "23334445555")
+    assert_equal "0000000000", attendee.phone_number
+  end
+
+  def test_it_throws_away_phone_numbers_that_are_too_short
+    attendee = Attendee.new(:phone_number => "222333444")
+    assert_equal "0000000000", attendee.phone_number
+  end
+
 end
+
